fb0a6301921ee9da1cd1337b35368cd96f52a1ac
Arman <armanjafary1@gmail.com>
2021-05-02 16:53:02 +0430

    bug fixed

--- resources/views/Product/products.blade.php
+++ resources/views/Product/products.blade.php
@@ -3,113 +3,129 @@
 
 <!--start main-->
 <main>
-
-    <div class="col-lg-12">
-
-    </div>
-    <div class="mt-5">
-        <div class="col-lg-12">
-            <div class="card position-absolute mt-5 text-center" style="left: 10px; z-index:1;">
-                <div class="row">
-                    <a href="#"> <img src="{{asset('assets/img/svg element/insta.svg')}}" alt="" class="shabak"></a>
-                    <a href="#"> <img src="{{asset('assets/img/svg element/insta.svg')}}" alt="" class="shabak"></a>
-                    <a href="#"> <img src="{{asset('assets/img/svg element/insta.svg')}}" alt="" class="shabak"></a>
-                    <a href="#"> <img src="{{asset('assets/img/svg element/wat.svg')}}" alt="" class="shabak"></a>
-                </div>
-            </div>
-        </div>
+	<div class="col-lg-12">
+		<div class="mt-5">
+			<div class="col-lg-12">
+				<div class="card position-absolute mt-5 text-center" style="left: 10px; z-index:1;">
+					<div class="row">
+						<a href="#"> <img src="{{asset('assets/img/svg element/insta.svg')}}" alt="" class="shabak"></a>
+						<a href="#"> <img src="{{asset('assets/img/svg element/insta.svg')}}" alt="" class="shabak"></a>
+						<a href="#"> <img src="{{asset('assets/img/svg element/insta.svg')}}" alt="" class="shabak"></a>
+						<a href="#"> <img src="{{asset('assets/img/svg element/wat.svg')}}" alt="" class="shabak"></a>
+					</div>
+				</div>
+			</div>
 
 
-        <img src="{{asset('assets/img/راهنمای-کامل-راه-اندازی-بوتیک-لباس3.jpg')}}" alt="" width="100%"
-            style="height: 20em; opacity:0.7; position:relative">
-    </div>
-    <div class="row">
+			<img src="{{asset('assets/img/راهنمای-کامل-راه-اندازی-بوتیک-لباس3.jpg')}}" alt="" width="100%"
+				style="height: 20em; opacity:0.7; position:relative">
+		</div>
+		<div class="row">
 
-    </div>
-    <div class="col-lg-12 ml-auto mr-auto col-8">
-        <div class="card-header text-center mt-5 card-header-product w-100 pt-5 pb-5"><a class="new-product">
-                WAHL </a></div>
-<div class="row">
-<div class="col-lg-3 mt-5">		
-				<div class="card text-center">
-				<img class="card-img-top" src="https://dkstatics-public.digikala.com/digikala-products/325689.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80" alt="هپل">
-					<div class="card-body">
-					<h5 class="card-title">ماشین اصلاح کیمی مدل KM-5017</h5>
-						<p class="card-text">191800تومان</p>
-					<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+		</div>
+		<div class="col-lg-12 ml-auto mr-auto col-8">
+			<div class="card-header text-center mt-5 card-header-product w-100 pt-5 pb-5"><a class="new-product">
+					WAHL </a></div>
+			<div class="row">
+				<div class="col-lg-3 mt-5">
+					<div class="card text-center">
+						<img class="card-img-top"
+							src="https://dkstatics-public.digikala.com/digikala-products/325689.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+							alt="هپل">
+						<div class="card-body">
+							<h5 class="card-title">ماشین اصلاح کیمی مدل KM-5017</h5>
+							<p class="card-text">191800تومان</p>
+							<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+						</div>
 					</div>
 				</div>
-	</div>
-	<div class="col-lg-3 mt-5">		
-				<div class="card text-center">
-				<img class="card-img-top" src="https://dkstatics-public.digikala.com/digikala-products/3883796.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80" alt="هپل">
-					<div class="card-body">
-					<h5 class="card-title">چرخ گوشت پارس خزر مدل MG-1600P</h5>
-						<p class="card-text">1999000تومان</p>
-					<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+				<div class="col-lg-3 mt-5">
+					<div class="card text-center">
+						<img class="card-img-top"
+							src="https://dkstatics-public.digikala.com/digikala-products/3883796.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+							alt="هپل">
+						<div class="card-body">
+							<h5 class="card-title">چرخ گوشت پارس خزر مدل MG-1600P</h5>
+							<p class="card-text">1999000تومان</p>
+							<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+						</div>
 					</div>
 				</div>
-	</div>
-	<div class="col-lg-3 mt-5">		
-				<div class="card text-center">
-				<img class="card-img-top" src="https://dkstatics-public.digikala.com/digikala-products/765534a781b4271f1343b65d334dd467fe5cd2d7_1606326066.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80" alt="هپل">
-					<div class="card-body">
-					<h5 class="card-title">فلاسک بونیتو مدل SS20Q گنجایش 2 لیتر</h5>
-						<p class="card-text">330000تومان</p>
-					<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+				<div class="col-lg-3 mt-5">
+					<div class="card text-center">
+						<img class="card-img-top"
+							src="https://dkstatics-public.digikala.com/digikala-products/765534a781b4271f1343b65d334dd467fe5cd2d7_1606326066.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+							alt="هپل">
+						<div class="card-body">
+							<h5 class="card-title">فلاسک بونیتو مدل SS20Q گنجایش 2 لیتر</h5>
+							<p class="card-text">330000تومان</p>
+							<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+						</div>
 					</div>
 				</div>
-	</div>
-	<div class="col-lg-3 mt-5">		
-				<div class="card text-center">
-				<img class="card-img-top" src="https://dkstatics-public.digikala.com/digikala-products/772887.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80" alt="هپل">
-					<div class="card-body">
-					<h5 class="card-title">
-کباب پز روگازی صبا مدل رعنا 1029 </h5>
-						<p class="card-text">125500تومان</p>
-					<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+				<div class="col-lg-3 mt-5">
+					<div class="card text-center">
+						<img class="card-img-top"
+							src="https://dkstatics-public.digikala.com/digikala-products/772887.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+							alt="هپل">
+						<div class="card-body">
+							<h5 class="card-title">
+								کباب پز روگازی صبا مدل رعنا 1029 </h5>
+							<p class="card-text">125500تومان</p>
+							<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+						</div>
 					</div>
 				</div>
-	</div>
-	<div class="col-lg-3 mt-5">		
-				<div class="card text-center">
-				<img class="card-img-top" src="https://dkstatics-public.digikala.com/digikala-products/120137399.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80" alt="هپل">
-					<div class="card-body">
-					<h5 class="card-title">نان پز عرشیا مدل CM 2523</h5>
-						<p class="card-text">870000تومان</p>
-					<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+				<div class="col-lg-3 mt-5">
+					<div class="card text-center">
+						<img class="card-img-top"
+							src="https://dkstatics-public.digikala.com/digikala-products/120137399.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+							alt="هپل">
+						<div class="card-body">
+							<h5 class="card-title">نان پز عرشیا مدل CM 2523</h5>
+							<p class="card-text">870000تومان</p>
+							<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+						</div>
 					</div>
 				</div>
-	</div>
-	<div class="col-lg-3 mt-5">		
-				<div class="card text-center">
-				<img class="card-img-top" src="https://dkstatics-public.digikala.com/digikala-products/120095929.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80" alt="هپل">
-					<div class="card-body">
-					<h5 class="card-title">زود پز برقی هایسونگ مدل5090</h5>
-						<p class="card-text">2399000تومان</p>
-					<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+				<div class="col-lg-3 mt-5">
+					<div class="card text-center">
+						<img class="card-img-top"
+							src="https://dkstatics-public.digikala.com/digikala-products/120095929.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+							alt="هپل">
+						<div class="card-body">
+							<h5 class="card-title">زود پز برقی هایسونگ مدل5090</h5>
+							<p class="card-text">2399000تومان</p>
+							<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+						</div>
 					</div>
 				</div>
-	</div>
-	<div class="col-lg-3 mt-5">		
-				<div class="card text-center">
-				<img class="card-img-top" src="https://dkstatics-public.digikala.com/digikala-products/2606031.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80" alt="هپل">
-					<div class="card-body">
-					<h5 class="card-title">هات داگ پز آریته مدل 186</h5>
-						<p class="card-text">697500تومان</p>
-					<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+				<div class="col-lg-3 mt-5">
+					<div class="card text-center">
+						<img class="card-img-top"
+							src="https://dkstatics-public.digikala.com/digikala-products/2606031.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+							alt="هپل">
+						<div class="card-body">
+							<h5 class="card-title">هات داگ پز آریته مدل 186</h5>
+							<p class="card-text">697500تومان</p>
+							<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+						</div>
 					</div>
 				</div>
-	</div>
-	<div class="col-lg-3 mt-5">		
-				<div class="card text-center">
-				<img class="card-img-top" src="https://dkstatics-public.digikala.com/digikala-products/2673358.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80" alt="هپل">
-					<div class="card-body">
-					<h5 class="card-title">کیک پز برقی گرانیتی سورنا</h5>
-						<p class="card-text">208000تومان</p>
-					<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+				<div class="col-lg-3 mt-5">
+					<div class="card text-center">
+						<img class="card-img-top"
+							src="https://dkstatics-public.digikala.com/digikala-products/2673358.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+							alt="هپل">
+						<div class="card-body">
+							<h5 class="card-title">کیک پز برقی گرانیتی سورنا</h5>
+							<p class="card-text">208000تومان</p>
+							<a href="" class="btn btn-primary pl-3 pr-3 pt-2 pb-2">اضافه به سبد خرید</a>
+						</div>
 					</div>
 				</div>
+			</div>
+		</div>
 	</div>
-</div>
+</main>
 @endsection
