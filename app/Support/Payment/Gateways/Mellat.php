2d1803af685514d01a31d26c57baa68ec5601f56
Arman <armanjafary1@gmail.com>
2021-06-10 16:58:04 +0430

    mellat bug fixedc

--- app/Support/Payment/Gateways/Mellat.php
+++ app/Support/Payment/Gateways/Mellat.php
@@ -102,7 +102,50 @@ class Mellat implements GatewayInterface
     }
     public function verify(Request $request)
     {
-        
+        if ($_POST['ResCode'] == '0') {
+            //--پرداخت در بانک باموفقیت بوده
+            $client = new nusoap_client('https://bpm.shaparak.ir/pgwchannel/services/pgw?wsdl');
+            $namespace='http://interfaces.core.sw.bps.com/';
+         
+            $terminalId				= "5453042";					// Terminal ID
+            $userName				= "iranturan123";					// Username
+            $userPassword			= "65916041";					// Password
+            $orderId 				= $request->input('SaleOrderId');		// Order ID
+            
+            $verifySaleOrderId 		= $request->input('SaleOrderId');
+            $verifySaleReferenceId 	= $request->input('SaleReferenceId');
+                    
+            $parameters = array(
+                'terminalId' => $terminalId,
+                'userName' => $userName,
+                'userPassword' => $userPassword,
+                'orderId' => $orderId,
+                'saleOrderId' => $verifySaleOrderId,
+                'saleReferenceId' => $verifySaleReferenceId);
+            // Call the SOAP method
+            $result = $client->call('bpVerifyRequest', $parameters, $namespace);
+            if($result == '0') {
+                //-- وریفای به درستی انجام شد٬ درخواست واریز وجه
+                // Call the SOAP method
+                $result = $client->call('bpSettleRequest', $parameters, $namespace);
+                if($result == '0') {
+                    //-- تمام مراحل پرداخت به درستی انجام شد.
+                    //-- آماده کردن خروجی
+                    echo 'The transaction was successful';
+                } else {
+                    //-- در درخواست واریز وجه مشکل به وجود آمد. درخواست بازگشت وجه داده شود.
+                    $client->call('bpReversalRequest', $parameters, $namespace);			
+                    echo 'Error : '. $result;
+                }
+            } else {
+                //-- وریفای به مشکل خورد٬ نمایش پیغام خطا و بازگشت زدن مبلغ
+                $client->call('bpReversalRequest', $parameters, $namespace);
+                echo 'Error : '. $result;
+            }
+        } else {
+            //-- پرداخت با خطا همراه بوده
+            echo 'Error : '. $_POST['ResCode'];
+        }
     }
     public function getName():string
     {
