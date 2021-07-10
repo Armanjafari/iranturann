f4856cb6a8b9e1763018b53dec361108a5854184
Arman <armanjafary1@gmail.com>
2021-07-10 14:12:22 +0430

    bug fixed

--- resources/views/Market/index.blade.php
+++ resources/views/Market/index.blade.php
@@ -4,63 +4,68 @@
     <!-- start add-product box-->
     <form action="{{ route('market.search') }}" method="get">
 
-    <div class="col-lg-12 d-flex justify-content-center">
-    <input type="search" name="query" id="" placeholder="دنبال چی می گردی؟" class="p-1 form-control serch-box">
-        </form>    
-</div>
-<div class="owl-carousel owl-theme mt-5 text-center" id="owl-mobile20">
-@forelse ($categories as $category)
-<div class="item"><a href="" class="Seller"> {{$category->persian_name}} </a></div>
+        <div class="col-lg-12 d-flex justify-content-center">
+            <input type="search" name="query" id="" placeholder="دنبال چی می گردی؟" class="p-1 form-control serch-box">
+    </form>
+    </div>
+    <div class="owl-carousel owl-theme mt-5 text-center" id="owl-mobile20">
+        @forelse ($categories as $category)
+        <div class="item"><a href="" class="Seller"> {{$category->persian_name}} </a></div>
 
-@empty
+        @empty
 
-@endforelse
-</div>
-<div class="col-lg-12 mt-5 pr-0 pl-0">
-  <div class="w-50 mr-auto ml-auto"> <a href=""><p class="discount">دیدن تمام محصولات این دسته بندی</p></a></div>
-    @forelse ($categories as $category)
-    @forelse ($category->products as $product)
-    <div class="owl-carousel owl-theme mt-2" id=owl-mobile12>
-        <form action="{{ route('market.add.product') }}" method="post">
-            @csrf
-        <div class="product-card mb-3">
-            <a class="w-100">
+        @endforelse
+    </div>
+    <div class="col-lg-12 mt-5 pr-0 pl-0">
+        <div class="w-50 mr-auto ml-auto"> <a href="">
+                <p class="discount">دیدن تمام محصولات این دسته بندی</p>
+            </a></div>
+        <div class="owl-carousel owl-theme mt-2" id=owl-mobile12>
+            @forelse ($categories as $category)
+            @forelse ($category->products as $product)
+            <form action="{{ route('market.add.product') }}" method="post">
+                @csrf
+                <div class="product-card mb-3">
+                    <a class="w-100">
 
 
-                <img src="{{ $product->images->first()->address ?? '#' }}" alt=""
-                    class="img-product-size mr-auto ml-auto">
-                <caption>
-                    <p class="mt-3 caption-product mb-0">{{$product->persian_title}}</p>
-                </caption>
-                <select name="product" style="display: none">
-                    <option value="{{$product->id}}"></option>
-                </select>
-                @if (!Auth::user()->market->products()->wherePure_id($product->id)->first())
-                <div class="text-center caption-product mt-1">
-                    <button type="submit" class="btn btn-primary caption-product p-1 mb-2"> فروشنده شوید</button>
+                        <img src="{{ $product->images->first()->address ?? '#' }}" alt=""
+                            class="img-product-size mr-auto ml-auto">
+                        <caption>
+                            <p class="mt-3 caption-product mb-0">{{$product->persian_title}}</p>
+                        </caption>
+                        <select name="product" style="display: none">
+                            <option value="{{$product->id}}"></option>
+                        </select>
+                        @if (!Auth::user()->market->products()->wherePure_id($product->id)->first())
+                        <div class="text-center caption-product mt-1">
+                            <button type="submit" class="btn btn-primary caption-product p-1 mb-2"> فروشنده
+                                شوید</button>
+                        </div>
+
+                        @else
+                        <div class="text-center caption-product mt-1">
+                            <button disabled class="btn btn-primary caption-product p-1 mb-2"> شما فروشنده هستید
+                            </button>
+                        </div>
+                        @endif
+                    </a>
                 </div>
+            </form>
+            @empty
+            @endforelse
 
-                @else
-                <div class="text-center caption-product mt-1">
-                    <button disabled class="btn btn-primary caption-product p-1 mb-2">شما فروشنده شوید</button>
-                </div>
-                @endif
+            @empty
+
+            @endforelse
+        </div>
+
+        <div class="text-center mt-4">
+            <a href="{{ route('Prodcut.registraition.form') }}" class="btn btn-success">
+                <i class="fa fa-plus"></i>
+                افزودن محصول
             </a>
         </div>
-        </form>
+
     </div>
-    @empty
-@endforelse
-
-@empty
-
-@endforelse
-<div class="text-center mt-4">
-    <a href="{{ route('Prodcut.registraition.form') }}" class="btn btn-success">
-       <i class="fa fa-plus"></i>
-       افزودن محصول
-    </a>
-    </div>
-
-  </div>
-  @endsection
+    @endsection
