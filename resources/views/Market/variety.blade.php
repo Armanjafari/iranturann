4f455b71882d46c3915228640586b089e0139806
Arman <armanjafary1@gmail.com>
2021-07-10 14:22:00 +0430

    bug fixec

--- resources/views/Market/variety.blade.php
+++ resources/views/Market/variety.blade.php
@@ -18,8 +18,8 @@
 </div>
 <div class="col-lg-12 mt-5 pr-0 pl-0">
   <div class="w-50 mr-auto ml-auto"> <a href=""><p class="discount">دیدن تمام محصولات این دسته بندی</p></a></div>
-    @forelse (Auth::user()->market->products as $product)
-    <div class="owl-carousel owl-theme mt-2" id=owl-mobile12>
+  <div class="owl-carousel owl-theme mt-2" id=owl-mobile12>  
+  @forelse (Auth::user()->market->products as $product)
         <form action="{{ route('market.variety.add.form') }}" method="GET">
             @csrf
         <div class="product-card mb-3">
@@ -40,10 +40,11 @@
             </a>
         </div>
         </form>
-    </div>
 @empty
 
 @endforelse
+</div>
+
 <div class="text-center mt-4">
     <a href="pre-registration.html" class="btn btn-success">
        <i class="fa fa-plus"></i>
