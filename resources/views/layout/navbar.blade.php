7d9412bf73019e8c38cc896090cf8de2865075e1
mehdi <mahdi13961234@gmail.com>
2021-07-17 15:26:50 +0430

    update profile

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -302,10 +302,16 @@
                             <img src="assets/img/download (4).png" alt="">
                             <span>خانه، آشپزخانه و ابزار</span>
                             <img class="item-icon" src="assets/img/download.png" alt=""></a></li>
-                           <div class="mt-2 mr-auto ml-3"> <a href="" class=""><i class="fas fa-user p-3"></i>
+                           <div class="mt-2 mr-auto ml-3"> <a href="" class=""><i class="fas fa-user fa-user1 p-3"></i>
                         </a>
-                        <div class="profile-box">
-                        
+                        <div class="profile-box p-3">
+                       <a href="">  <i class="fas fa-user-circle ml-2"></i>زهرا رنجبر</a>
+                       <hr>
+                       <a href="">  <i class="fas fa-user fa-user2 ml-2"></i>مشاهده حساب کاربری</a>
+                       <hr>
+                       <a href="">  <i class="fas fa-scroll fa-scroll1 ml-2"></i>سفارش های من</a>
+                       <hr>
+                       <a href="">  <i class="fas fa-sign-out-alt fa-sign-out-alt1 ml-2"></i>خروج از حساب کاربری</a>
                         </div>
                         </div>
                         
@@ -317,11 +323,12 @@
                                 
                                 <a class="text-center btn-logo" href="{{ route('login_with_code') }}"><img src="assets/img/svg element/ورود.svg" alt="" class="pt-1 pl-1">ورود/ثبت نام</a>
                         @endguest
-                        @auth                    
-                        <button type="button" class="btn-logo">
+                        @auth       
+                        <a class="text-center btn-logo" href="{{ route('logout') }}">خروج از حساب کاربری</a>       
+                        <!-- <button type="button" class="btn-logo">
                             <i class="far fa-user"></i>
                             <a class="text-center" href="{{ route('logout') }}">خروج از حساب کاربری</a>
-                        </button>
+                        </button> -->
                         @endauth
                 </ul><!-- End Menu Level 1 --> 
             </div>
@@ -339,7 +346,7 @@
             <img src="assets/img/svg element/Icon awesome-shopping-cart.svg" alt="" class="svg-shopp ml-4">
             </a> --}}
             @guest
-          <a href="{{ route('login_with_code') }}">  <i class="far fa-user mt-2"></i></a>  
+          <a href="{{ route('login_with_code') }}">  <i class="far fa-user fa-user1 mt-2"></i></a>  
             @endguest
            @auth
           <a href="{{ route('logout') }}" class="btn-logo1">خروج</a>  
