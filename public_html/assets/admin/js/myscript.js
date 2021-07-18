4c80f734824b97f7a701fc70a459fb35db2916a1
mehdi <mahdi13961234@gmail.com>
2021-07-18 12:42:35 +0430

    update category box fix

--- public_html/assets/admin/js/myscript.js
+++ public_html/assets/admin/js/myscript.js
@@ -17,9 +17,9 @@ function buildDropDown(values) {
 }
 
 //Capture the event when user types into the search box
-window.addEventListener('input', function () {
-    filter(search.value.trim().toLowerCase())
-})
+// window.addEventListener('input', function () {
+//     filter(search.value.trim().toLowerCase())
+// })
 
 //For every word entered by the user, check if the symbol starts with that word
 //If it does show the symbol, else hide it
@@ -73,43 +73,40 @@ function filter(word) {
 const nodeArray = (selector, parent=document) => [].slice.call(parent.querySelectorAll(selector));
 
 //  checkboxes of interest 
-const allThings = nodeArray('input');
-
-
+// const allThings = nodeArray('input');
 //  global listener
-addEventListener('change', e => {
-  let check = e.target;
-
+// addEventListener('change', e => {
+//   let check = e.target;
   //  exit if change event did not come from 
   //  our list of allThings 
-  if(allThings.indexOf(check) === -1) return;
+//   if(allThings.indexOf(check) === -1) return;
 
   //  check/unchek children (includes check itself)
-  const children = nodeArray('input', check.parentNode);
-  children.forEach(child => child.checked = check.checked);
+//   const children = nodeArray('input', check.parentNode);
+//   children.forEach(child => child.checked = check.checked);
   
   //  traverse up from target check
-  while(check){
+//   while(check){
     
     //  find parent and sibling checkboxes (quick'n'dirty)
-    const parent   = (check.closest(['ul']).parentNode).querySelector('input');
-    const siblings = nodeArray('input', parent.closest('li').querySelector(['ul']));
+    // const parent   = (check.closest(['ul']).parentNode).querySelector('input');
+    // const siblings = nodeArray('input', parent.closest('li').querySelector(['ul']));
 
     //  get checked state of siblings
     //  are every or some siblings checked (using Boolean as test function) 
-    const checkStatus = siblings.map(check => check.checked);
-    const every  = checkStatus.every(Boolean);
-    const some = checkStatus.some(Boolean);   
+//     const checkStatus = siblings.map(check => check.checked);
+//     const every  = checkStatus.every(Boolean);
+//     const some = checkStatus.some(Boolean);   
     
-    //  check parent if all siblings are checked
-    //  set indeterminate if not all and not none are checked
-    parent.checked = every;   
-    parent.indeterminate = !every && every !== some;
+//     //  check parent if all siblings are checked
+//     //  set indeterminate if not all and not none are checked
+//     parent.checked = every;   
+//     parent.indeterminate = !every && every !== some;
 
-    //  prepare for nex loop
-    check = check != parent ? parent : 0;
-  }
-})
+//     //  prepare for nex loop
+//     check = check != parent ? parent : 0;
+//   }
+// })
 $(function () {
     var $preview = $("#preview");
 
