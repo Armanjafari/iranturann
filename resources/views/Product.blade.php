e895bddde9cbd001f208990b3ce35e3f52ae46e8
mehdi <mahdi13961234@gmail.com>
2021-07-25 14:58:48 +0430

    updated bog fix

--- resources/views/Product.blade.php
+++ resources/views/Product.blade.php
@@ -187,8 +187,7 @@
                 <div class="col-md-12 pr-0 pl-0">
                     <nav>
                         <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
-                            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home"
-                                role="tab" aria-controls="nav-home" aria-selected="true">مشخصات</a>
+
                             <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile"
                                 role="tab" aria-controls="nav-profile" aria-selected="false">توضیحات</a>
                             <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact"
