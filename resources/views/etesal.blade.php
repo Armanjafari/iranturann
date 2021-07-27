fd1b6cc435a2e9426351ee5d90206030581afdee
mehdi <mahdi13961234@gmail.com>
2021-07-27 18:08:27 +0430

    Single product app

--- resources/views/etesal.blade.php
+++ resources/views/etesal.blade.php
@@ -1,8 +1,33 @@
 @extends('Market.layout.master')
 @section('content')
-<main>
-     <div class="content6">
+<main class="mt-3">
+     <div class="content6 pr-3 pl-3">
        <img src="img/01.png" alt="" class="product-size">
-     </div>
+        <caption>
+          <p>پیراهن مردانه مدلadidas</p>
+        </caption>
+        <p>
+        Adidas shirt
+        </p>
+       
+        <div>
+          <p>
+            توضیحات محصول:
+          </p>
+          <span>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam molestiae perspiciatis ullam explicabo, error quos sed ipsum perferendis architecto rerum quae recusandae eligendi cupiditate praesentium voluptates similique aspernatur quia soluta.
+          </span><br><br>
+          <span>قیمت مرجع :</span>
+          <span class="link-brand">125,000تومان</span>
+        </div>
+        <div class="mt-3">
+          <span>برند:</span>
+          <span class="link-brand">آدیداس</span>
+        </div>
+        <div class="mt-3">
+          <span>دسته بندی:</span>
+          <span class="link-brand">پوشاک</span>
+        </div>
+      </div>
 </main>
 @endsection
