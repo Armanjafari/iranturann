e155a4ff5ec63702af8a9c7476d59f697fea0dfe
Arman <armanjafary1@gmail.com>
2021-07-28 21:36:37 +0430

    bug fixed

--- resources/views/mobile/layout/footer.blade.php
+++ resources/views/mobile/layout/footer.blade.php
@@ -19,7 +19,7 @@
       </div>
       <div class="col-lg-4 mt-4 text-center">
         <caption>درصورتی که نیاز به مشاوره دارید با ما تماس حاصل نمایید</caption>
-        <p class="mt-2">09359214103</p>
+        <p class="mt-2">{{$market->user->phone_number}}</p>
       </div>
     </div>
     <hr>
@@ -35,7 +35,6 @@
         <p>حریم خصوصی</p>
       </div>
       <div class="col-lg-2 text-center col-sm-6 col-6 mt-lg-0 mt-3">
-        <p>با ایران توران</p>
         <p>تماس باما</p>
         <p>درباره ما</p>
       </div>
@@ -52,19 +51,6 @@
       </div>
       <div class="col-lg-3 text-center col-sm-6 col-6 mt-lg-0 mt-3">
         <img src="{{ asset('assets/img/svg element/download.svg') }}" alt="" class="img-app">
-      </div>
-      <div class="col-lg-3 text-sm-right text-lg-center text-right col-sm-6 col-6 mt-5">
-        <p>همراه با ایران توران در شبکه های اجتماعی</p>
-      </div>
-      <div class="col-lg-2 col-sm-4 col-6 mt-5 col-md-4">
-        <div class="card border-dark">
-          <div class="card-body text-right p-3">
-            <img src="{{ asset('assets/img/svg element/Icon awesome-facebook-f.svg') }}" alt="" class="facebook">
-            <img src="{{ asset('assets/img/svg element/Icon awesome-twitter.svg') }}" alt="" class="mr-3 twitter">
-            <img src="{{ asset('assets/img/svg element/Icon awesome-youtube.svg') }}" alt="" class="mr-3 youtube">
-            <img src="{{ asset('assets/img/svg element/Icon awesome-instagram.svg') }}" alt="" class="mr-3 instagram">
-          </div>
-        </div>
       </div>
       <div class="col-lg-5 mr-auto mt-lg-0 mt-3 col-sm-12 col-12 ml-sm-3">
         <a href="https://trustseal.enamad.ir/?id=153838&code=7SuhHcVpcGxSaUSzRNTr">
