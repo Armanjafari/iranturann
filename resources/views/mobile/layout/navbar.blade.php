7290ab9d57cf190e0f66176d707bd3049ad2c6eb
mehdi <mahdi13961234@gmail.com>
2021-09-04 20:04:45 +0430

    updated bog fix

--- resources/views/mobile/layout/navbar.blade.php
+++ resources/views/mobile/layout/navbar.blade.php
@@ -4,12 +4,12 @@
 <div class="content bg-light">
     <ul class="pr-0 mb-0">
         <div class="row">
-            <div class="col-3 text-center box-menu">
+            <div class="col-2 text-center box-menu">
                 <a href="{{route('mobile.show.market',$market->id)}}"> <i class="fas fa-home fas1 {{url()->current() == route('mobile.show.market',$market->id) ? 'active-menu' : '' }}"><p class="mt-2">خانه</p></i>
                     
                 </a>
             </div>
-            <div class="col-3 text-center box-menu">
+            <div class="col-4 text-center box-menu">
                 <a href="{{route('mobile.category.list',$market->id)}}" class=""> <i class="fas fa-list-alt fas1 {{url()->current() == route('mobile.category.list',$market->id) ? 'active-menu' : '' }}"> <p class="mt-2">دسته بندی</p></i>
 
                 </a>
