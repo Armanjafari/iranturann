a11c6ead0a327cbb42e8541387c12bc08323e747
Arman <armanjafary1@gmail.com>
2021-09-05 17:20:02 +0430

    bug fixed

--- app/Http/Controllers/mobile/SearchController.php
+++ app/Http/Controllers/mobile/SearchController.php
@@ -5,20 +5,41 @@ namespace App\Http\Controllers\mobile;
 use App\Http\Controllers\Controller;
 use App\Market;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class SearchController extends Controller
 {
-    public function search(Market $market)
+    public function search(Market $market , Request $request)
     {
-        
+        // TODO refactor this moudle
+        $query = $request->input('query');
+        $pures = [];
         foreach ($market->products as $product) {
-            return $product->pure()->where('persian_title' ,'LIKE','%' . 'samsung' . '%')
-            ->orWhere('persian_title' ,'LIKE','%' .'samsung')
-            ->orWhere('persian_title' ,'LIKE','samsung'.'%' )
-            ->orWhere('title' ,'LIKE','%' . 'samsung' . '%')
-            ->orWhere('title' ,'LIKE','samsung' . '%')
-            ->orWhere('title' ,'LIKE','%' . 'samsung')->get();
+            $product = $product->pure()->where('persian_title' ,'LIKE','%' . $query . '%')
+            ->orWhere('persian_title' ,'LIKE','%' .$query)
+            ->orWhere('persian_title' ,'LIKE',$query.'%' )
+            ->orWhere('title' ,'LIKE','%' . $query . '%')
+            ->orWhere('title' ,'LIKE',$query . '%')
+            ->orWhere('title' ,'LIKE','%' . $query)->get();
+            array_push($pures ,$product);
         }
+
+        Arr::flatten($pures);
+        $products = [];
+        foreach ($pures as $key) {
+            foreach ($key as $product) {
+                foreach ($product->products as $product) {
+                    if ($product->market_id == $market->id) {
+                    foreach ($product->fulls as $full) {
+                        array_push($products , $full);
+                    }
+                    }
+                }
+            }
+        }
+        $products = collect($products);
+        $products = $products->unique('product_id');
+        dd($products);
 
     }
 }
