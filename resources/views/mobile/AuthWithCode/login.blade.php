df0ae0dceb4b80726fda12df975f86354ad84669
mehdi <mahdi13961234@gmail.com>
2021-09-09 13:09:58 +0430

    updated bog fix login

--- resources/views/mobile/AuthWithCode/login.blade.php
+++ resources/views/mobile/AuthWithCode/login.blade.php
@@ -10,7 +10,8 @@
                 <p class="font12">برای ورود یا ثبت نام شماره موبایل خود را وارد نمایید</p>
                <input type="tel" name="phone_number" placeholder="شماره موبایل خود راوارد کنید" class="border-0 input-style">
             <input type="submit" class="mt-3 loginsystem" value="ورود به سیستم">
-               </form> 
+               </form>
+               <a href="" class="font14  mt-3 Rules-link">ثبت نام</a> 
            </div>
             <!-- end login -->
            </div>
